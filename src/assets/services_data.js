e42b4b96437c60671ead4f9767d884f2978578f7
Aldair Rocha <aldairmurilo000@gmail.com>
2024-09-10 00:58:36 -0300

    correcao css e aplicacoes, certificados

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,33 +1,37 @@
-const Services_Data =[
+const Services_Data = [
     {
-        s_no:"01",
-        s_name:"HTML & CSS",
-        s_desc:"Gustavo Guanabara - YT"
+      s_no: "01",
+      s_name: "HTML & CSS",
+      s_desc: "Gustavo Guanabara - YT",
+      link: "https://www.youtube.com/watch?v=Ejkb_YpuHWs&list=PLHz_AreHm4dkZ9-atkcmcBaMZdmLHft8n"
     },
     {
-        s_no:"02",
-        s_name:"My SQL",
-        s_desc:"Udemy - Andamento"
+      s_no: "02",
+      s_name: "My SQL",
+      s_desc: "Udemy - Andamento",
     },
     {
-        s_no:"03",
-        s_name:"JavaScript",
-        s_desc:"Udemy - Andamento"
+      s_no: "03",
+      s_name: "JavaScript",
+      s_desc: "Udemy - Andamento"
     },
     {
-        s_no:"04",
-        s_name:"React JS",
-        s_desc:"Udemy - Andamento"
+      s_no: "04",
+      s_name: "React JS",
+      s_desc: "Udemy - Andamento"
     },
     {
-        s_no:"05",
-        s_name:"PhP",
-        s_desc:"Udemy - Andamento"
+      s_no: "05",
+      s_name: "PhP",
+      s_desc: "Udemy - Andamento"
     },
     {
-        s_no:"06",
-        s_name:"Cypress",
-        s_desc:"Concluído"
-    },
-]
-export default Services_Data
+      s_no: "06",
+      s_name: "Cypress",
+      s_desc: "Concluído",
+      link:"../../assets/Certificado_de_Conclusao_Cypress.pdf"
+    }
+  ];
+  
+  export default Services_Data;
+  
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,30 +1,54 @@
-import React from 'react'
-import './Services.css'
-import Services_Data from '../../assets/services_data' 
-
+import React from 'react';
+import './Services.css';
+import Services_Data from '../../assets/services_data';
+import CertificadoCypress from '../../assets/Certificado_de_Conclusao_Cypress.pdf'; // Importando o arquivo
 
 const Services = () => {
   return (
     <div id='services' className='services'>
-      <div className="services-tittle">
+      <div className="services-title">
         <h1>Meus Cursos</h1>
         <img src="#" alt="" />
       </div>
       <div className="services-container">
-        {Services_Data.map((services,index)=>{
-          return <div key={index} className="services-format">
-            <h3>{services.s_no}</h3>
-            <h2>{services.s_name}</h2>
-            <p>{services.s_desc}</p>
-            <div className="services-readmore">
-              <p><a className='link-cert' href="">Certificado</a></p>
-           
+        {Services_Data.map((service, index) => {
+          return (
+            <div key={index} className="services-format">
+              <h3>{service.s_no}</h3>
+              <h2>{service.s_name}</h2>
+              <p>{service.s_desc}</p>
+              <div className="services-readmore">
+                {service.s_name === "Cypress" ? (
+                  <p>
+                    <a
+                      className='link-cert'
+                      href={CertificadoCypress}
+                      download="Certificado_de_Conclusao_Cypress.pdf"
+                    >
+                    Certificado
+                    </a>
+                  </p>
+                ) : service.link ? (
+                  <p>
+                    <a
+                      className='link-cert'
+                      href={service.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Certificado
+                    </a>
+                  </p>
+                ) : (
+                  <p>Sem certificado disponível</p>
+                )}
+              </div>
             </div>
-          </div>
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
 
-export default Services
+export default Services;
